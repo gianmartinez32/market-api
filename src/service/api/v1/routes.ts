9be30a5c47b97e54e76f75af358aaf454096e5d7
Gian Franco Martínez Villa <gian.martinez@devitech.com.co>
2023-10-20 10:20:30 -0500

    feat: validacion de 404

--- src/service/api/v1/routes.ts
+++ src/service/api/v1/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import PlayerRoutes from '@api/v1/player/player.route';
 import ProductoRoute from '@api/v1/producto/producto.route';
 import VentaRoute from '@api/v1/venta/venta.route';
@@ -8,5 +8,18 @@ const v1Router: Router = Router();
 v1Router.use('/player', PlayerRoutes);
 v1Router.use('/producto', ProductoRoute);
 v1Router.use('/sales', VentaRoute);
+v1Router.use((req, res, next) => {
+  const error:any = new Error('Ruta no encontrada');
+  error.status = 404;
+  next(error);
+});
+
+v1Router.use((error:any, req:Request, res:Response, next:any) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 export default v1Router;
